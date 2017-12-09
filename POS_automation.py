22175dd91aac7bf4c87e492a99ce14c69442af55
jleatham <j.leatham@gmail.com>
2017-12-08 20:56:10 -0600

    velocity test

--- POS_automation.py
+++ POS_automation.py
@@ -249,7 +249,7 @@ def to_csv_from_json_v2(FILES,ALLCSV,NONERRORCSV):
                     text = f.read()
                 FILEDATA = StringIO(text)
                 df = pd.DataFrame() # create empty dataframe                       
-                df = pd.read_csv(FILEDATA,low_memory=False, usecols=["POS Transaction ID/Unique ID","Posted Date",	'POS Split Adjusted Value USD', 'Product ID','POS SCA Mode','Ship-To Source Customer Name','Sold-To Source Customer Name',"End Customer Source Customer Name","End Customer CR Party ID","Salesrep Email","Salesrep Name"])
+                df = pd.read_csv(FILEDATA,low_memory=False, usecols=["POS Transaction ID/Unique ID","Posted Date",	'POS Split Adjusted Value USD', 'Product ID','POS SCA Mode','Ship-To Source Customer Name','Sold-To Source Customer Name',"End Customer Source Customer Name","End Customer CR Party ID","Salesrep Email","Salesrep Name","Salesrep #"])
                 shutil.move(file, old_pos_file_path+filename)
                 print ("processed: "+filename)
             except Exception as e:
@@ -302,7 +302,7 @@ def to_csv_from_json_v2(FILES,ALLCSV,NONERRORCSV):
             json_accounts.append(str(account)) #problem with some accounts not being strings, but which?  and how?
         json_email.append(str(v["email"])) 
     print("done with getting json")
-    df = master_df[(master_df['End Customer Source Customer Name'].astype(str).isin(json_accounts) | master_df['Ship-To Source Customer Name'].astype(str).isin(json_accounts) | master_df['Sold-To Source Customer Name'].astype(str).isin(json_accounts) | master_df["Salesrep Email"].astype(str).isin(json_email))]
+    df = master_df[(master_df['End Customer Source Customer Name'].astype(str).isin(json_accounts) | master_df['Ship-To Source Customer Name'].astype(str).isin(json_accounts) | master_df['Sold-To Source Customer Name'].astype(str).isin(json_accounts) | master_df["Salesrep Email"].astype(str).isin(json_email) | master_df["Salesrep #"].astype(str).isin(json_email) )]
     print("done with narrowing search results")
 
     frames = [] #re-initialize frames so we can concat below df's
@@ -318,7 +318,7 @@ def to_csv_from_json_v2(FILES,ALLCSV,NONERRORCSV):
         for account in v["accounts"]:
             ACCOUNTS.append(str(account))
         FALSE = v["false_positives"]
-        results = df[(df['End Customer Source Customer Name'].astype(str).isin(ACCOUNTS) | df['Ship-To Source Customer Name'].astype(str).isin(ACCOUNTS) | df['Sold-To Source Customer Name'].astype(str).isin(ACCOUNTS)) & ~df["Salesrep Email"].str.contains(EMAIL) & ~df['End Customer Source Customer Name'].astype(str).isin(FALSE)] 
+        results = df[(df['End Customer Source Customer Name'].astype(str).isin(ACCOUNTS) | df['Ship-To Source Customer Name'].astype(str).isin(ACCOUNTS) | df['Sold-To Source Customer Name'].astype(str).isin(ACCOUNTS)) & ( ~df["Salesrep Email"].str.contains(EMAIL) | ~df["Salesrep #"].str.contains(EMAIL) ) & ~df['End Customer Source Customer Name'].astype(str).isin(FALSE)] 
         #results.index.names = ['POS ID']
         results.rename(columns = {'POS Transaction ID/Unique ID':'POS ID','Posted Date':'Date','POS Split Adjusted Value USD':'$$$','Ship-To Source Customer Name':'Ship-To','Sold-To Source Customer Name':'Sold-To','End Customer Source Customer Name':'End Customer','End Customer CR Party ID':'Party ID','POS SCA Mode':'Mode','Salesrep Name':'AM Credited'}, inplace=True)
         #results.loc[:,'Sort Here'] = EMAIL
@@ -329,7 +329,7 @@ def to_csv_from_json_v2(FILES,ALLCSV,NONERRORCSV):
         results['Date'] = pd.to_datetime(results['Date'], errors='coerce')
 
 
-        non_error_results = df[df["Salesrep Email"].str.contains(EMAIL) ]#& len(df.index)<20
+        non_error_results = df[df["Salesrep Email"].str.contains(EMAIL) | df["Salesrep #"].str.contains(EMAIL) ]#& len(df.index)<20
         #non_error_results.index.names = ['POS ID']
         non_error_results.rename(columns = {'POS Transaction ID/Unique ID':'POS ID','Posted Date':'Date','POS Split Adjusted Value USD':'$$$','Ship-To Source Customer Name':'Ship-To','Sold-To Source Customer Name':'Sold-To','End Customer Source Customer Name':'End Customer','End Customer CR Party ID':'Party ID', 'POS SCA Mode':'Mode','Salesrep Name':'AM Credited'}, inplace=True)                    
         non_error_results["Sort Here"] = EMAIL
