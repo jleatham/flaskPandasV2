8d73287ab3a3051893a55e4f366d6a05934ee573
jleatham <j.leatham@gmail.com>
2017-12-08 21:07:30 -0600

    fix

--- POS_automation.py
+++ POS_automation.py
@@ -318,7 +318,7 @@ def to_csv_from_json_v2(FILES,ALLCSV,NONERRORCSV):
         for account in v["accounts"]:
             ACCOUNTS.append(str(account))
         FALSE = v["false_positives"]
-        results = df[(df['End Customer Source Customer Name'].astype(str).isin(ACCOUNTS) | df['Ship-To Source Customer Name'].astype(str).isin(ACCOUNTS) | df['Sold-To Source Customer Name'].astype(str).isin(ACCOUNTS)) & ( ~df["Salesrep Email"].str.contains(EMAIL) | ~df["Salesrep #"].str.contains(EMAIL) ) & ~df['End Customer Source Customer Name'].astype(str).isin(FALSE)] 
+        results = df[(df['End Customer Source Customer Name'].astype(str).isin(ACCOUNTS) | df['Ship-To Source Customer Name'].astype(str).isin(ACCOUNTS) | df['Sold-To Source Customer Name'].astype(str).isin(ACCOUNTS)) & ~df["Salesrep Email"].str.contains(EMAIL) & ~df["Salesrep #"].str.contains(EMAIL)  & ~df['End Customer Source Customer Name'].astype(str).isin(FALSE)] 
         #results.index.names = ['POS ID']
         results.rename(columns = {'POS Transaction ID/Unique ID':'POS ID','Posted Date':'Date','POS Split Adjusted Value USD':'$$$','Ship-To Source Customer Name':'Ship-To','Sold-To Source Customer Name':'Sold-To','End Customer Source Customer Name':'End Customer','End Customer CR Party ID':'Party ID','POS SCA Mode':'Mode','Salesrep Name':'AM Credited'}, inplace=True)
         #results.loc[:,'Sort Here'] = EMAIL
